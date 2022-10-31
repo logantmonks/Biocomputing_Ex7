e36b99431bdadfcf2d8915181b495004417b988d
Peter Martin <pmarti23@nd.edu>
2022-10-30 20:10:41 -0400

    Corrected mistakes in code and saved .txt file

--- Exercise07_PeterMartin.R
+++ Exercise07_PeterMartin.R
@@ -5,13 +5,13 @@
 setwd("~/Desktop/Rfiles/Biocomputing/Biocomputing_Ex7") # Set the working directory for the script
 
 iris=read.csv(file="iris.csv",header=TRUE) # Reads in file
-write.table(x=iris,file="iris.txt",sep="\t",quote=F) # Generates a tab-delimited .txt file from the read
+write.table(x=iris,file="iris.txt",sep="\t",quote=F,row.names = FALSE) # Generates a tab-delimited .txt file from the read
 
 # Creating components of list and combining into one list
 vec1=seq(from=100,to=1000,by=100)
 df1=data.frame(team_name=c("Notre Dame","UNLV"),score=c(44,21))
 number<-999
 mat1=matrix(data = 1:50, nrow=10, ncol=5, byrow = TRUE)
-vec2=c(16,24,26)
+vec2=c("p","o","m")
 
-list1=list(vec1,df1,number,mat1,vec2)
+list1=list(vec1,df1,number,mat1,vec2) # Combines all elements above into one list of length 5
