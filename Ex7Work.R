be633614f874f854cb31f04c8a9c24028185ef1a
Sara Bennett <sarab3862@icloud.com>
2022-11-03 23:49:53 -0400

    Bennett Ex7 Submission

--- Ex7Work.R
+++ Ex7Work.R
@@ -1,1 +1,25 @@
 #Exercise 7 - Sara Bennett
+
+#1 Comma del to tab
+iris <- read.csv("iris.csv")
+write.table(iris, file = "iris.txt", sep = "\t")
+
+#2
+
+#vector of 10 elements 100-1000
+nums = seq(from = 100, to = 1000, by = 100)
+
+#DF of Football scores
+football<- data.frame(teams = c("Notre Dame", "UNLV"), scores = c(44,21))
+
+#Matrix of integers 1-50
+ints = matrix(data=1:50,nrow=10,ncol=5)
+
+#vector of 3 letters
+vec3 = c("A","B","C")
+
+#Create list of all 5 elements
+listStuff <- list(nums, football, 999, ints, vec3)
+
+#Display list
+listStuff
